833d0701996069591c8561ac4e67e705e2662d42
ramiro zein <ramirozeincontrerasrangel@gmail.com>
2026-04-19 01:22:51 -0600

    feat: implement JWT-based session management and update Capacitor configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ramirozein.zound',
   appName: 'Zound',
-  webDir: 'public', // Unused essentially because we are wrapping a server URL
-  bundledWebRuntime: false,
+  webDir: 'public',
   server: {
     url: 'https://musica.ramirozein.me',
     cleartext: false
